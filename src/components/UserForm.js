7b32491fd0560a8c00e9ae57b4cf168b78ba3836
ZPPD <zhana.p.d@gmail.com>
2019-01-02 12:14:07 -0500

    user details component

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import FormUserDetails from "./FormUserDetails";
 
 export class UserForm extends Component {
   state = {
@@ -10,8 +11,49 @@ export class UserForm extends Component {
     city: "",
     bio: ""
   };
+
+  // Procees to next step
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1
+    });
+  };
+
+  // Go back to prec step
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1
+    });
+  };
+
+  // Handle fields change
+  handleChange = input => e => {
+    this.setState({ [input]: e.target.value });
+  };
+
   render() {
-    return <div />;
+    const { step } = this.state;
+    const { firstName, lastName, occupation, city, bio } = this.state;
+    const values = { firstName, lastName, occupation, city, bio };
+
+    switch (step) {
+      case 1:
+        return (
+          <FormUserDetails
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return <h1>FormPersonalDetails</h1>;
+      case 3:
+        return <h1>Confirm</h1>;
+      case 4:
+        return <h1>Success</h1>;
+    }
   }
 }
 
